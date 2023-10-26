5a23fff0be2446dc05eadf8f64ded9cf105400a3
whr819987540 <819987540@qq.com>
2023-10-26 15:48:11 +0800

    TorrentCommunication封装了RPCClient提供的服务

--- rpc_client.py
+++ rpc_client.py
@@ -23,10 +23,90 @@ from threading import Thread
 import requests
 from time import sleep
 import signal
-import sys
+from torch import distributed as dist
+import torch
+import numpy as np
+from typing import Union
+import logging
 
 
+class TorrentCommunication:
+    """
+        封装RPCClient(python)提供的服务
+        只提供两个通信原语与一个停止上传原语
+        通信原语1: 
+            如果是server, bt_broadcast将数据广播给所有client 
+                1) 生成torrent
+                2) 对数据进行seed
+                2) 将torrent广播(这个广播操作借用了pytorch.distributed提供的broadcast)给所有client, client收到后会以torrent为参数通过bt_recv操作开始下载
+            如果是client, bt_broadcast负责接收torrent
+            无论是server还是client, 这个函数都会返回torrent
+            
+        通信原语2: bt_recv通过bt进行下载
+            1) 以torrent为参数调用RPCClient.start_downloading
+            2) 再调用RPCServer.start_downloading
 
+        停止上传原语: server/client在完成下载后, 默认都会继续上传, 该原语终止上传行为
+    """
+
+    def __init__(self, logger: logging.Logger):
+        """
+            实例化RPCClient
+        """
+        self.rpc_client = RPCClient(logger)
+        self.logger = logger
+
+    def bt_broadcast(self, data_path: Union[str, None]):
+        """
+            For server, data_path is str and this function returns torrent. If this function failed, an Exception will be raised.
+            
+            For client, data_path is None and this function returns torrent.
+        """
+        rank = dist.get_rank()
+        self.logger.debug(f"execute bt_broadcast: {data_path}")
+        if rank == 0:
+            # create torrent
+            torrent, status = self.rpc_client.create_torrent(data_path)
+            if not status:
+                raise Exception("create torrent error")
+
+            # TODO: run seed and send torrent in parallel
+            # seed
+            text, status = self.rpc_client.start_seeding(torrent)
+            if not status:
+                raise Exception(text)
+
+            # send torrent
+            torrent_tensor = torch.from_numpy(np.frombuffer(torrent, np.uint8))
+            # TODO: 这里存在两个发送，增加了时延，通过别的发送方式可以优化
+            # size
+            dist.broadcast(torch.tensor([torrent_tensor.shape[0]], dtype=torch.int64), 0)
+            # data
+            dist.broadcast(torrent_tensor, 0)
+            return torrent
+        else:
+            # size
+            torrent_size = torch.empty(1, dtype=torch.int64)
+            dist.broadcast(torrent_size, 0)
+            # data
+            torrent_tensor = torch.empty(torrent_size[0], dtype=torch.uint8)
+            dist.broadcast(torrent_tensor, 0)
+            torrent = torrent_tensor.numpy().tobytes()
+
+        return torrent
+
+    def bt_recv(self, torrent: bytes):
+        # TODO: 这个函数可以是非阻塞的
+        downloading_output, status = self.rpc_client.start_downloading(torrent)
+        if not status:
+            raise Exception("bt_recv downloading error")
+        else:
+            return downloading_output
+
+    def stop_seeding(self, torrent: bytes):
+        text, status = self.rpc_client.stop_seeding(torrent)
+        if not status:
+            raise Exception(text)
 
 
 
